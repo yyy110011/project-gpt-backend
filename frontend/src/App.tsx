1b847a98d791376c9b88363d9b99a0e7a51d8aec
Ryan Yao <yyy1100112@gmail.com>
2023-03-18 13:31:14 +0800

    Add front end.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ChatGPT from './ChatGPT';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ChatGPT />
+    </div>
+  );
+};
+
+export default App;
