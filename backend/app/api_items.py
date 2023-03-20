1532934404f3883edb9deda8778205470842d5e3
Ryan Yao <yyy1100112@gmail.com>
2023-03-20 17:19:53 +0800

    Update api_items.

--- backend/app/api_items.py
+++ backend/app/api_items.py
@@ -1,43 +1,6 @@
 from pydantic import BaseModel
 
-user_db = {
-    "0": "test1"
-}
-
-# user name as key
-chat_message_db = {
-    "0":{
-        "c++ programming": [
-            {"role": "system", "content": "You are so good,"},
-            {"role": "user", "content": "You are so good,"},
-            {"role": "assistant", "content": "You are so good,"},
-        ],
-        "c++ 2": [
-    
-        ],
-        "c++ 3": [
-    
-        ],
-    },
-    "2":{
-        "c++ programming": [
-    
-        ],
-        "c++ 2": [
-    
-        ],
-        "c++ 3": [
-    
-        ],
-    },
-}
-
-sim_db = {
-    
-}
-
 class UserInfo(BaseModel):
-    user_id: str
     user_name: str
 
 class ChatMessage(BaseModel):
