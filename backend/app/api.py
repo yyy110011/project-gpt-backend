e51889bfe6f61b763b3e3eb50a82dc083fb1c59f
Ryan Yao <yyy1100112@gmail.com>
2023-03-20 17:27:00 +0800

    Add db control.

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,3 +1,8 @@
+import asyncio
+import asyncpg
+from app.database import Database
+from app.models.models import Models
+
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,27 +29,44 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
+from .env import *
+@app.on_event("startup")
+async def startup_event():
+    app.state.db = Database(
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_DATABASE,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    app.state.data_model = Models(app.state.db)
+    await app.state.db.connect()
+
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
 
 @app.get("/v1/get_users/", tags=["user"])
 async def get_users():
-    return user_db
+    ret = await app.state.data_model.Users().get_users()
+    return ret
 
 @app.delete("/v1/delete_user/{user_id}", tags=["user"])
 async def delete_user(user_id: str):
-    if user_db.get(user_id):
-        user_db.pop(user_id)
+    ret = await app.state.data_model.Users().delete_user(user_id)
+    return ret
 
 @app.post("/v1/create_user/", tags=["user"])
 async def create_user(user_info: UserInfo):
-    if not user_db.get(user_info.user_id):
-        user_db[user_info.user_id] = str(user_info.user_name)
-        chat_message_db[user_info.user_id] = {}
-        return {"message": "Create fail.", "result": user_db}
-    else:
-        return {"message": "Create fail."}
+    ret = await app.state.data_model.Users().create_user(user_info.user_name)
+    return ret
+    # if not user_db.get(user_info.user_id):
+    #     user_db[user_info.user_id] = str(user_info.user_name)
+    #     chat_message_db[user_info.user_id] = {}
+    #     return {"message": "Create fail.", "result": user_db}
+    # else:
+    #     return {"message": "Create fail."}
 
 @app.delete("/v1/delete_chat/{user_id}_{chat_id}", tags=["chat"])
 async def delete_chat(user_id, chat_id):
